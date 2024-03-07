d3bccd4f2a4c14a38189560b545dd87ef6c8c0bd
Memeruler_k <104277893+Memeruler-k@users.noreply.github.com>
2024-03-07 17:14:50 +0200

    Create SlotCrash.java

---
+++ src/main/java/com/example/addon/modules/SlotCrash.java
@@ -0,0 +1,10 @@
+package com.moon.addon.modules;
+
+import com.moon.addon.Addon;
+import meteordevelopment.meteorclient.systems.modules.Module;
+
+public class SlotCrash extends Module {
+    public SlotCrash() {
+        super(Addon.CATEGORY, "Slot Crash", "Method for crashing servers using slots.");
+    }
+}
