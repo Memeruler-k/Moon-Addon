8222568bb2fe0b1f8e603e726c0997c34459e4f0
Memeruler_k <104277893+Memeruler-k@users.noreply.github.com>
2024-03-07 17:10:42 +0200

    Create OpenWaterESP.java

---
+++ src/main/java/com/example/addon/modules/OpenWaterESP.java
@@ -0,0 +1,10 @@
+package com.moon.addon.modules;
+
+import com.moon.addon.Addon;
+import meteordevelopment.meteorclient.systems.modules.Module;
+
+public class OpenWaterESP extends Module {
+    public OpenWaterESP() {
+        super(Addon.CATEGORY, "Open Water ESP", "Shows open water with ESP.");
+    }
+}
