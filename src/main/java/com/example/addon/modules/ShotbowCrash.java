bef706d8d1bf0fcdd1952c2a3d5aa6ca9921752f
Memeruler_k <104277893+Memeruler-k@users.noreply.github.com>
2024-03-07 17:13:59 +0200

    Create ShotbowCrash.java

---
+++ src/main/java/com/example/addon/modules/ShotbowCrash.java
@@ -0,0 +1,10 @@
+package com.moon.addon.modules;
+
+import com.moon.addon.Addon;
+import meteordevelopment.meteorclient.systems.modules.Module;
+
+public class ShotbowCrash extends Module {
+    public ShotbowCrash() {
+        super(Addon.CATEGORY, "Shotbow Crash", "An unknown crash method for servers.");
+    }
+}
