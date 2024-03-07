d641a0fb65bcbd8e22cf86c1a1b1124bba3bfd46
Memeruler_k <104277893+Memeruler-k@users.noreply.github.com>
2024-03-07 17:20:03 +0200

    Create UwU.java

---
+++ src/main/java/com/example/addon/modules/UwU.java
@@ -0,0 +1,10 @@
+package com.moon.addon.modules;
+
+import com.moon.addon.Addon;
+import meteordevelopment.meteorclient.systems.modules.Module;
+
+public class UwU extends Module {
+    public UwU() {
+        super(Addon.CATEGORY, "UwU", "Automatically UwU on others.");
+    }
+}
