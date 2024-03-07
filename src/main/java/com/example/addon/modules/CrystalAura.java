2ce23aa21d6c38f8cee9056dbb931c0897846801
Memeruler_k <104277893+Memeruler-k@users.noreply.github.com>
2024-03-07 16:47:22 +0200

    Create CrystalAura.java

---
+++ src/main/java/com/example/addon/modules/CrystalAura.java
@@ -0,0 +1,10 @@
+package com.moon.addon.modules;
+
+import com.moon.addon.Addon;
+import meteordevelopment.meteorclient.systems.modules.Module;
+
+public class CrystalAura extends Module {
+    public CrystalAura() {
+        super(Addon.CATEGORY, "Crystal Aura", "Automatically places and destroys crystals. Useful for Crystal PVP.");
+    }
+}
