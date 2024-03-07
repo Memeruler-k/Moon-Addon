0e4b82e5b74c93a6addb1b75e3dfc85e73591c3f
Memeruler_k <104277893+Memeruler-k@users.noreply.github.com>
2024-03-07 17:18:56 +0200

    Create TreeBot.java

---
+++ src/main/java/com/example/addon/modules/TreeBot.java
@@ -0,0 +1,10 @@
+package com.moon.addon.modules;
+
+import com.moon.addon.Addon;
+import meteordevelopment.meteorclient.systems.modules.Module;
+
+public class TreeBot extends Module {
+    public TreeBot() {
+        super(Addon.CATEGORY, "Tree Bot", "Automatically farms trees for you.");
+    }
+}
