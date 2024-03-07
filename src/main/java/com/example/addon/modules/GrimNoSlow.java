3a2383f669d0dc780d18d1d6ccf195fba161607d
Memeruler_k <104277893+Memeruler-k@users.noreply.github.com>
2024-03-07 16:51:00 +0200

    Create GrimNoSlow.java

---
+++ src/main/java/com/example/addon/modules/GrimNoSlow.java
@@ -0,0 +1,10 @@
+package com.moon.addon.modules;
+
+import com.moon.addon.Addon;
+import meteordevelopment.meteorclient.systems.modules.Module;
+
+public class GrimNoSlow extends Module {
+    public GrimNoSlow() {
+        super(Addon.CATEGORY, "Grim No Slow", "No Slow bypass for Grim.");
+    }
+}
