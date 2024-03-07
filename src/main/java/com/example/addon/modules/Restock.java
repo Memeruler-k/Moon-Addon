74f813e00e1b9b6243e716c709ded628e0823cdf
Memeruler_k <104277893+Memeruler-k@users.noreply.github.com>
2024-03-07 17:13:05 +0200

    Create Restock.java

---
+++ src/main/java/com/example/addon/modules/Restock.java
@@ -0,0 +1,10 @@
+package com.moon.addon.modules;
+
+import com.moon.addon.Addon;
+import meteordevelopment.meteorclient.systems.modules.Module;
+
+public class Restock extends Module {
+    public Restock() {
+        super(Addon.CATEGORY, "Restock", "Automatically restocks Storages.");
+    }
+}
