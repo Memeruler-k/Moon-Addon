6b99ed262e101783a68b777e56c888cab8e8b915
Memeruler_k <104277893+Memeruler-k@users.noreply.github.com>
2024-03-07 17:08:06 +0200

    Create MultiAura.java

---
+++ src/main/java/com/example/addon/modules/MultiAura.java
@@ -0,0 +1,10 @@
+package com.moon.addon.modules;
+
+import com.moon.addon.Addon;
+import meteordevelopment.meteorclient.systems.modules.Module;
+
+public class MultiAura extends Module {
+    public MultiAura() {
+        super(Addon.CATEGORY, "Multi Aura", "Allows you to attack multiple targets.");
+    }
+}
