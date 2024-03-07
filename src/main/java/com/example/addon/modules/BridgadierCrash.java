e43b894451e0c4f37bfbe7095fa5f8a5142ee94c
Memeruler_k <104277893+Memeruler-k@users.noreply.github.com>
2024-03-07 16:41:54 +0200

    Create BridgadierCrash.java

---
+++ src/main/java/com/example/addon/modules/BridgadierCrash.java
@@ -0,0 +1,10 @@
+package com.moon.addon.modules;
+
+import com.moon.addon.Addon;
+import meteordevelopment.meteorclient.systems.modules.Module;
+
+public class BridgadierCrash extends Module {
+    public BridgadierCrash() {
+        super(Addon.CATEGORY, "Bridgadier Crash", "Method for crashing servers.");
+    }
+}
