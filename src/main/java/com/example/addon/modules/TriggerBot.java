a0b8ebf64005f0b9fdf5adc3cd157d9b8c8f431d
Memeruler_k <104277893+Memeruler-k@users.noreply.github.com>
2024-03-07 17:19:33 +0200

    Create TriggerBot.java

---
+++ src/main/java/com/example/addon/modules/TriggerBot.java
@@ -0,0 +1,10 @@
+package com.moon.addon.modules;
+
+import com.moon.addon.Addon;
+import meteordevelopment.meteorclient.systems.modules.Module;
+
+public class TriggerBot extends Module {
+    public TriggerBot() {
+        super(Addon.CATEGORY, "Trigger Bot", "Automatically attacks your target who hit you.");
+    }
+}
