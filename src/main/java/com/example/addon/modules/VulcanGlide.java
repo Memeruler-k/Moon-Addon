69c79d2e06ec20649d6baf50aafb2eb3f6d0497d
Memeruler_k <104277893+Memeruler-k@users.noreply.github.com>
2024-03-07 17:20:34 +0200

    Create VulcanGlide.java

---
+++ src/main/java/com/example/addon/modules/VulcanGlide.java
@@ -0,0 +1,10 @@
+package com.moon.addon.modules;
+
+import com.moon.addon.Addon;
+import meteordevelopment.meteorclient.systems.modules.Module;
+
+public class VulcanGlide extends Module {
+    public VulcanGlide() {
+        super(Addon.CATEGORY, "Vulcan Glide", "Glide bypass for Vulcan.");
+    }
+}
