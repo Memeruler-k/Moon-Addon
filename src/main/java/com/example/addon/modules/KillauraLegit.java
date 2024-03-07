195cb52bba3a0a59163c416cb79de898e7b0bde3
Memeruler_k <104277893+Memeruler-k@users.noreply.github.com>
2024-03-07 17:04:04 +0200

    Create KillauraLegit.java

---
+++ src/main/java/com/example/addon/modules/KillauraLegit.java
@@ -0,0 +1,10 @@
+package com.moon.addon.modules;
+
+import com.moon.addon.Addon;
+import meteordevelopment.meteorclient.systems.modules.Module;
+
+public class KillauraLegit extends Module {
+    public KillauraLegit() {
+        super(Addon.CATEGORY, "Kill Aura Legit", "Killaura but legit.");
+    }
+}
