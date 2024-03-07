1d5049b5283e92b289549d0c61f7709ea5e3a3d2
Memeruler_k <104277893+Memeruler-k@users.noreply.github.com>
2024-03-07 16:40:56 +0200

    Create BowAimbot.java

---
+++ src/main/java/com/example/addon/modules/BowAimbot.java
@@ -0,0 +1,10 @@
+package com.moon.addon.modules;
+
+import com.moon.addon.Addon;
+import meteordevelopment.meteorclient.systems.modules.Module;
+
+public class BowAimbot extends Module {
+    public BowAimbot() {
+        super(Addon.CATEGORY, "Bow Aimbot", "Bow Aimbot from Wurst.");
+    }
+}
