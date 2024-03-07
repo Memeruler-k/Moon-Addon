83ffb6878dce3f6877a04f37d65630bbd1ac490e
Memeruler_k <104277893+Memeruler-k@users.noreply.github.com>
2024-03-07 16:50:15 +0200

    Create ForceOP.java

---
+++ src/main/java/com/example/addon/modules/ForceOP.java
@@ -0,0 +1,10 @@
+package com.moon.addon.modules;
+
+import com.moon.addon.Addon;
+import meteordevelopment.meteorclient.systems.modules.Module;
+
+public class ForceOP extends Module {
+    public ForceOP() {
+        super(Addon.CATEGORY, "Force OP", "Makes you OP. Works in cracked servers with LoginSecurity.");
+    }
+}
