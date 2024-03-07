028afe36311431cbb494790847e9be50df8ecdc0
Memeruler_k <104277893+Memeruler-k@users.noreply.github.com>
2024-03-07 16:46:41 +0200

    Create CreativeFlight.java

---
+++ src/main/java/com/example/addon/modules/CreativeFlight.java
@@ -0,0 +1,10 @@
+package com.moon.addon.modules;
+
+import com.moon.addon.Addon;
+import meteordevelopment.meteorclient.systems.modules.Module;
+
+public class CreativeFlight extends Module {
+    public CreativeFlight() {
+        super(Addon.CATEGORY, "Creative Flight", "Fly like your in creative mode.");
+    }
+}
