6c8164d62da6363d4fdbdc58b27a872f8f79cffd
Memeruler_k <104277893+Memeruler-k@users.noreply.github.com>
2024-03-07 16:39:28 +0200

    Create BedAuraV2.java

---
+++ src/main/java/com/example/addon/modules/BedAuraV2.java
@@ -0,0 +1,10 @@
+package com.moon.addon.modules;
+
+import com.moon.addon.Addon;
+import meteordevelopment.meteorclient.systems.modules.Module;
+
+public class BedAuraV2 extends Module {
+    public BedAuraV2() {
+        super(Addon.CATEGORY, "Bed Aura V2", "An improved version of Bed Aura.");
+    }
+}
