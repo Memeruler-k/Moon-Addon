de3669c6cc2cd839661c499334d2282dbd6bd8e7
Memeruler_k <104277893+Memeruler-k@users.noreply.github.com>
2024-03-07 17:11:23 +0200

    Create PortalGUI.java

---
+++ src/main/java/com/example/addon/modules/PortalGUI.java
@@ -0,0 +1,10 @@
+package com.moon.addon.modules;
+
+import com.moon.addon.Addon;
+import meteordevelopment.meteorclient.systems.modules.Module;
+
+public class PortalGUI extends Module {
+    public PortalGUI() {
+        super(Addon.CATEGORY, "Portal GUI", "Allows you to open GUIs when inside a portal.");
+    }
+}
