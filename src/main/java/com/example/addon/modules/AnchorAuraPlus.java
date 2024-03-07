785b4288112541ac51a08ca1e64d2a255c2dfabc
Memeruler_k <104277893+Memeruler-k@users.noreply.github.com>
2024-03-07 16:35:55 +0200

    Update and rename ModuleExample.java to AnchorAuraPlus.java

---
+++ src/main/java/com/example/addon/modules/AnchorAuraPlus.java
@@ -0,0 +1,10 @@
+package com.moon.addon.modules;
+
+import com.moon.addon.Addon;
+import meteordevelopment.meteorclient.systems.modules.Module;
+
+public class AnchorAuraPlus extends Module {
+    public AnchorAuraPlus() {
+        super(Addon.CATEGORY, "Anchor Aura+", "An improved version of Anchor Aura");
+    }
+}
