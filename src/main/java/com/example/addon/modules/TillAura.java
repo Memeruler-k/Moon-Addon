40e241a2730fafca330b378cb517473a97283938
Memeruler_k <104277893+Memeruler-k@users.noreply.github.com>
2024-03-07 17:17:15 +0200

    Create TillAura.java

---
+++ src/main/java/com/example/addon/modules/TillAura.java
@@ -0,0 +1,10 @@
+package com.moon.addon.modules;
+
+import com.moon.addon.Addon;
+import meteordevelopment.meteorclient.systems.modules.Module;
+
+public class TillAura extends Module {
+    public TillAura() {
+        super(Addon.CATEGORY, "Till Aura", "Automatically turns dirt etc into farmland.");
+    }
+}
