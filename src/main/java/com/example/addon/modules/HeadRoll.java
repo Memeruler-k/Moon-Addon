35e206a12957eefbed719fa2ad7d325b282bdf0b
Memeruler_k <104277893+Memeruler-k@users.noreply.github.com>
2024-03-07 16:54:56 +0200

    Create HeadRoll.java

---
+++ src/main/java/com/example/addon/modules/HeadRoll.java
@@ -0,0 +1,10 @@
+package com.moon.addon.modules;
+
+import com.moon.addon.Addon;
+import meteordevelopment.meteorclient.systems.modules.Module;
+
+public class HeadRoll extends Module {
+    public HeadRoll() {
+        super(Addon.CATEGORY, "Head Roll", "Rolls your head at a spefic speed.");
+    }
+}
