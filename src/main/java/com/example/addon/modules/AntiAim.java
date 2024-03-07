02fd98b78aeecb2a9522bc8dd9fa96a544c93ee8
Memeruler_k <104277893+Memeruler-k@users.noreply.github.com>
2024-03-07 16:36:44 +0200

    Create AntiAim.java

---
+++ src/main/java/com/example/addon/modules/AntiAim.java
@@ -0,0 +1,10 @@
+package com.moon.addon.modules;
+
+import com.moon.addon.Addon;
+import meteordevelopment.meteorclient.systems.modules.Module;
+
+public class AntiAim extends Module {
+    public AntiAim() {
+        super(Addon.CATEGORY, "Anti Aim", "Makes your enemies kill themselves");
+    }
+}
