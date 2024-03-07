a952ba34ef763761db77435274c8c6d9a997bf55
Memeruler_k <104277893+Memeruler-k@users.noreply.github.com>
2024-03-07 16:54:14 +0200

    Create HandNoClip.java

---
+++ src/main/java/com/example/addon/modules/HandNoClip.java
@@ -0,0 +1,10 @@
+package com.moon.addon.modules;
+
+import com.moon.addon.Addon;
+import meteordevelopment.meteorclient.systems.modules.Module;
+
+public class HandNoClip extends Module {
+    public HandNoClip() {
+        super(Addon.CATEGORY, "Hand No Clip", "Allows you to interact with blocks you have chosen.");
+    }
+}
