2ac9c0dcc65a6f514ccb8f691a069fa22995f8e4
Memeruler_k <104277893+Memeruler-k@users.noreply.github.com>
2024-03-07 16:40:18 +0200

    Create BonemealAura.java

---
+++ src/main/java/com/example/addon/modules/BonemealAura.java
@@ -0,0 +1,10 @@
+package com.moon.addon.modules;
+
+import com.moon.addon.Addon;
+import meteordevelopment.meteorclient.systems.modules.Module;
+
+public class BonemealAura extends Module {
+    public BonemealAura() {
+        super(Addon.CATEGORY, "Bonemeal Aura", "Automatically bonemeals nearby seeds and plants.");
+    }
+}
