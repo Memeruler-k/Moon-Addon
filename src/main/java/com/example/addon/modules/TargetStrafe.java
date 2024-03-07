6bcadb7c16de03df4159a4127dddefde269075c4
Memeruler_k <104277893+Memeruler-k@users.noreply.github.com>
2024-03-07 17:18:24 +0200

    Create TargetStrafe.java

---
+++ src/main/java/com/example/addon/modules/TargetStrafe.java
@@ -0,0 +1,10 @@
+package com.moon.addon.modules;
+
+import com.moon.addon.Addon;
+import meteordevelopment.meteorclient.systems.modules.Module;
+
+public class TargetStrafe extends Module {
+    public TargetStrafe() {
+        super(Addon.CATEGORY, "Target Strafe", "Automatically circles around your target.");
+    }
+}
