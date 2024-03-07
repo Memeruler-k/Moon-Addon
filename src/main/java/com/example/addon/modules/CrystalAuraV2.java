0f2f43bc0094f3e1f9344ada41dc1c365f710ec0
Memeruler_k <104277893+Memeruler-k@users.noreply.github.com>
2024-03-07 16:48:16 +0200

    Create CrystalAuraV2.java

---
+++ src/main/java/com/example/addon/modules/CrystalAuraV2.java
@@ -0,0 +1,10 @@
+package com.moon.addon.modules;
+
+import com.moon.addon.Addon;
+import meteordevelopment.meteorclient.systems.modules.Module;
+
+public class CrystalAuraV2 extends Module {
+    public CrystalAuraV2() {
+        super(Addon.CATEGORY, "Crystal Aura V2", "An improved version of Crystal Aura.");
+    }
+}
