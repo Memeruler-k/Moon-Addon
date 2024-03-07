45c6225d2454c99827acd725c9285c28ff805093
Memeruler_k <104277893+Memeruler-k@users.noreply.github.com>
2024-03-07 16:51:41 +0200

    Create GrimSpeedMine.java

---
+++ src/main/java/com/example/addon/modules/GrimSpeedMine.java
@@ -0,0 +1,10 @@
+package com.moon.addon.modules;
+
+import com.moon.addon.Addon;
+import meteordevelopment.meteorclient.systems.modules.Module;
+
+public class GrimSpeedMine extends Module {
+    public GrimSpeedMine() {
+        super(Addon.CATEGORY, "Grim Speed Mine", "Speed Mine bypass for Grim.");
+    }
+}
