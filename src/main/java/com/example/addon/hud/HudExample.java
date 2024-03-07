208c717074b6b2d929c2bf774edb88e45b64be76
Memeruler_k <104277893+Memeruler-k@users.noreply.github.com>
2024-03-07 16:10:16 +0200

    Update HudExample.java

--- src/main/java/com/example/addon/hud/HudExample.java
+++ src/main/java/com/example/addon/hud/HudExample.java
@@ -7,7 +7,7 @@ import meteordevelopment.meteorclient.systems.hud.HudRenderer;
 import meteordevelopment.meteorclient.utils.render.color.Color;
 
 public class HudExample extends HudElement {
-    public static final HudElementInfo<HudExample> INFO = new HudElementInfo<>(Addon.HUD_GROUP, "example", "HUD element example.", HudExample::new);
+    public static final HudElementInfo<HudExample> INFO = new HudElementInfo<>(Addon.HUD_GROUP, "Target HUD", "Displays target's info when entering combat.", HudExample::new);
 
     public HudExample() {
         super(INFO);
