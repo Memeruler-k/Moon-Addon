f32843f5f42e89620c6bc86bf03d32c021c2ffc9
Memeruler_k <104277893+Memeruler-k@users.noreply.github.com>
2024-03-07 15:53:08 +0200

    Update Addon.java

--- src/main/java/com/example/addon/Addon.java
+++ src/main/java/com/example/addon/Addon.java
@@ -1,8 +1,8 @@
-package com.example.addon;
+package com.moon.addon;
 
-import com.example.addon.commands.CommandExample;
-import com.example.addon.hud.HudExample;
-import com.example.addon.modules.ModuleExample;
+import com.moon.addon.commands.CommandExample;
+import com.moon.addon.hud.HudExample;
+import com.moon.addon.modules.ModuleExample;
 import com.mojang.logging.LogUtils;
 import meteordevelopment.meteorclient.addons.MeteorAddon;
 import meteordevelopment.meteorclient.commands.Commands;
@@ -14,12 +14,12 @@ import org.slf4j.Logger;
 
 public class Addon extends MeteorAddon {
     public static final Logger LOG = LogUtils.getLogger();
-    public static final Category CATEGORY = new Category("Example");
-    public static final HudGroup HUD_GROUP = new HudGroup("Example");
+    public static final Category CATEGORY = new Category("Moon");
+    public static final HudGroup HUD_GROUP = new HudGroup("Moon");
 
     @Override
     public void onInitialize() {
-        LOG.info("Initializing Meteor Addon Template");
+        LOG.info("Initializing Moon Addon");
 
         // Modules
         Modules.get().add(new ModuleExample());
@@ -38,6 +38,6 @@ public class Addon extends MeteorAddon {
 
     @Override
     public String getPackage() {
-        return "com.example.addon";
+        return "com.moon.addon";
     }
 }
