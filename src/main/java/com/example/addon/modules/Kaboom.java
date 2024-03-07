d52ed817c7a1797e1415e071c51b27d7f8740238
Memeruler_k <104277893+Memeruler-k@users.noreply.github.com>
2024-03-07 17:02:32 +0200

    Create Kaboom.java

---
+++ src/main/java/com/example/addon/modules/Kaboom.java
@@ -0,0 +1,10 @@
+package com.moon.addon.modules;
+
+import com.moon.addon.Addon;
+import meteordevelopment.meteorclient.systems.modules.Module;
+
+public class Kaboom extends Module {
+    public Kaboom() {
+        super(Addon.CATEGORY, "Kaboom", "Makes you explode. REQUIRES CREATIVE MODE.");
+    }
+}
