06282066cf598d3e1307e38bb1c0e4367f516978
Memeruler_k <104277893+Memeruler-k@users.noreply.github.com>
2024-03-07 16:38:35 +0200

    Create BedAura.java

---
+++ src/main/java/com/example/addon/modules/BedAura.java
@@ -0,0 +1,10 @@
+package com.moon.addon.modules;
+
+import com.moon.addon.Addon;
+import meteordevelopment.meteorclient.systems.modules.Module;
+
+public class BedAura extends Module {
+    public BedAura() {
+        super(Addon.CATEGORY, "Bed Aura", "Automatically destroys nearby beds.");
+    }
+}
