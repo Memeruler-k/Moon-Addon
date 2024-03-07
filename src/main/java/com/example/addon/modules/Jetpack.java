675467cd64e80a3011f65bdbb85c10d83e20603b
Memeruler_k <104277893+Memeruler-k@users.noreply.github.com>
2024-03-07 16:59:24 +0200

    Create Jetpack.java

---
+++ src/main/java/com/example/addon/modules/Jetpack.java
@@ -0,0 +1,10 @@
+package com.moon.addon.modules;
+
+import com.moon.addon.Addon;
+import meteordevelopment.meteorclient.systems.modules.Module;
+
+public class Jetpack extends Module {
+    public Jetpack() {
+        super(Addon.CATEGORY, "Jetpack", "Fly like you have a jetpack on.");
+    }
+}
