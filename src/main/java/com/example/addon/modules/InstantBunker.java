3b8c2cef76e9f29d085c386c67b17ccf0419b480
Memeruler_k <104277893+Memeruler-k@users.noreply.github.com>
2024-03-07 16:57:54 +0200

    Create InstantBunker.java

---
+++ src/main/java/com/example/addon/modules/InstantBunker.java
@@ -0,0 +1,10 @@
+package com.moon.addon.modules;
+
+import com.moon.addon.Addon;
+import meteordevelopment.meteorclient.systems.modules.Module;
+
+public class InstantBunker extends Module {
+    public InstantBunker() {
+        super(Addon.CATEGORY, "Instant Bunker", "Builds a bunker around you with spefic blocks.");
+    }
+}
