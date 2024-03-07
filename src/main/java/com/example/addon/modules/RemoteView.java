dfd0225b5d217f05aff7b686d9fb1c9e72c409f4
Memeruler_k <104277893+Memeruler-k@users.noreply.github.com>
2024-03-07 17:12:29 +0200

    Create RemoteView.java

---
+++ src/main/java/com/example/addon/modules/RemoteView.java
@@ -0,0 +1,10 @@
+package com.moon.addon.modules;
+
+import com.moon.addon.Addon;
+import meteordevelopment.meteorclient.systems.modules.Module;
+
+public class RemoteView extends Module {
+    public RemoteView() {
+        super(Addon.CATEGORY, "Remote View", "View other player's POV.");
+    }
+}
