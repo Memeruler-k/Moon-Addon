99b5dd8b545df790f2b81b55e700ea3b3d00d95c
Memeruler_k <104277893+Memeruler-k@users.noreply.github.com>
2024-03-07 17:11:55 +0200

    Create ReachPlus.java

---
+++ src/main/java/com/example/addon/modules/ReachPlus.java
@@ -0,0 +1,10 @@
+package com.moon.addon.modules;
+
+import com.moon.addon.Addon;
+import meteordevelopment.meteorclient.systems.modules.Module;
+
+public class ReachPlus extends Module {
+    public ReachPlus() {
+        super(Addon.CATEGORY, "Reach+", "Reach but with bypasses.");
+    }
+}
