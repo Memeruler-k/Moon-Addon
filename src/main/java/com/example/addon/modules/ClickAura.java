73332a81ec39ba2c7e9feaa15878d291e5bba1e9
Memeruler_k <104277893+Memeruler-k@users.noreply.github.com>
2024-03-07 16:42:30 +0200

    Create ClickAura.java

---
+++ src/main/java/com/example/addon/modules/ClickAura.java
@@ -0,0 +1,10 @@
+package com.moon.addon.modules;
+
+import com.moon.addon.Addon;
+import meteordevelopment.meteorclient.systems.modules.Module;
+
+public class ClickAura extends Module {
+    public ClickAura() {
+        super(Addon.CATEGORY, "Click Aura", "Same as killaura but different.");
+    }
+}
