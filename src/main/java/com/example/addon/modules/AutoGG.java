708a96126fc55a254fc0e6b63b82e950741da548
Memeruler_k <104277893+Memeruler-k@users.noreply.github.com>
2024-03-07 16:37:36 +0200

    Create AutoGG.java

---
+++ src/main/java/com/example/addon/modules/AutoGG.java
@@ -0,0 +1,10 @@
+package com.moon.addon.modules;
+
+import com.moon.addon.Addon;
+import meteordevelopment.meteorclient.systems.modules.Module;
+
+public class AutoGG extends Module {
+    public AutoGG() {
+        super(Addon.CATEGORY, "Auto GG", "Automatically GGs.");
+    }
+}
