43207382f92b786829f0198ecbdd655b912d7918
Memeruler_k <104277893+Memeruler-k@users.noreply.github.com>
2024-03-07 17:07:15 +0200

    Create MountBypass.java

---
+++ src/main/java/com/example/addon/modules/MountBypass.java
@@ -0,0 +1,10 @@
+package com.moon.addon.modules;
+
+import com.moon.addon.Addon;
+import meteordevelopment.meteorclient.systems.modules.Module;
+
+public class MountBypass extends Module {
+    public MountBypass() {
+        super(Addon.CATEGORY, "Mount Bypass", "Allows you to dupe using Donkeys. Only works on 1.15.2 or below.");
+    }
+}
