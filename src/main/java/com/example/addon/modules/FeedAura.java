18042efb1686944bfb03b549824a80712b055fcf
Memeruler_k <104277893+Memeruler-k@users.noreply.github.com>
2024-03-07 16:48:49 +0200

    Create FeedAura.java

---
+++ src/main/java/com/example/addon/modules/FeedAura.java
@@ -0,0 +1,10 @@
+package com.moon.addon.modules;
+
+import com.moon.addon.Addon;
+import meteordevelopment.meteorclient.systems.modules.Module;
+
+public class FeedAura extends Module {
+    public FeedAura() {
+        super(Addon.CATEGORY, "Feed Aura", "Automatically feeds nearby animals.");
+    }
+}
