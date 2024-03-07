712dc6a53793b4fa43c08e36a789138dfe0e3173
Memeruler_k <104277893+Memeruler-k@users.noreply.github.com>
2024-03-07 17:04:53 +0200

    Create Liquids.java

---
+++ src/main/java/com/example/addon/modules/Liquids.java
@@ -0,0 +1,10 @@
+package com.moon.addon.modules;
+
+import com.moon.addon.Addon;
+import meteordevelopment.meteorclient.systems.modules.Module;
+
+public class Liquids extends Module {
+    public Liquids() {
+        super(Addon.CATEGORY, "Liquids", "Allows you to place blocks in liquids.");
+    }
+}
