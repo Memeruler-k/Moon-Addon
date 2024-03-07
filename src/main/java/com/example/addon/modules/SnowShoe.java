de645820b24eb2131f464f11334e10edb8535314
Memeruler_k <104277893+Memeruler-k@users.noreply.github.com>
2024-03-07 17:15:40 +0200

    Create SnowShoe.java

---
+++ src/main/java/com/example/addon/modules/SnowShoe.java
@@ -0,0 +1,10 @@
+package com.moon.addon.modules;
+
+import com.moon.addon.Addon;
+import meteordevelopment.meteorclient.systems.modules.Module;
+
+public class SnowShoe extends Module {
+    public SnowShoe() {
+        super(Addon.CATEGORY, "Snow Shoe", "Allows you to walk on powdered snow.");
+    }
+}
