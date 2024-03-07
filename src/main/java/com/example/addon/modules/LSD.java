5fc1ebdf555f7cd79c5e7884ccad978ecdb6928e
Memeruler_k <104277893+Memeruler-k@users.noreply.github.com>
2024-03-07 17:05:28 +0200

    Create LSD.java

---
+++ src/main/java/com/example/addon/modules/LSD.java
@@ -0,0 +1,10 @@
+package com.moon.addon.modules;
+
+import com.moon.addon.Addon;
+import meteordevelopment.meteorclient.systems.modules.Module;
+
+public class LSD extends Module {
+    public LSD() {
+        super(Addon.CATEGORY, "LSD", "Makes minecraft look weird.");
+    }
+}
