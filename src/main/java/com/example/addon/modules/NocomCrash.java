ba40b7e46bd4d90a337fdfb7aa3887c5a5f56131
Memeruler_k <104277893+Memeruler-k@users.noreply.github.com>
2024-03-07 17:09:58 +0200

    Create NocomCrash.java

---
+++ src/main/java/com/example/addon/modules/NocomCrash.java
@@ -0,0 +1,10 @@
+package com.moon.addon.modules;
+
+import com.moon.addon.Addon;
+import meteordevelopment.meteorclient.systems.modules.Module;
+
+public class NocomCrash extends Module {
+    public NocomCrash() {
+        super(Addon.CATEGORY, "Nocom Crash", "Lags servers using the Nocom Exploit. Only works on 1.18.2 or below.");
+    }
+}
