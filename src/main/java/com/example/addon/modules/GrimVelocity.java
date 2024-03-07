e8b63efce3e688818ba93064b57d1d1525a128ee
Memeruler_k <104277893+Memeruler-k@users.noreply.github.com>
2024-03-07 16:52:15 +0200

    Create GrimVelocity.java

---
+++ src/main/java/com/example/addon/modules/GrimVelocity.java
@@ -0,0 +1,10 @@
+package com.moon.addon.modules;
+
+import com.moon.addon.Addon;
+import meteordevelopment.meteorclient.systems.modules.Module;
+
+public class GrimVelocity extends Module {
+    public GrimVelocity() {
+        super(Addon.CATEGORY, "Grim Velocity", "Velocity bypass for Grim.");
+    }
+}
