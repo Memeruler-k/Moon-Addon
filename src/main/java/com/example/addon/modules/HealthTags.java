385825747cf69f9b82f61d43d9aac8e365f384f1
Memeruler_k <104277893+Memeruler-k@users.noreply.github.com>
2024-03-07 16:55:36 +0200

    Create HealthTags.java

---
+++ src/main/java/com/example/addon/modules/HealthTags.java
@@ -0,0 +1,10 @@
+package com.moon.addon.modules;
+
+import com.moon.addon.Addon;
+import meteordevelopment.meteorclient.systems.modules.Module;
+
+public class HealthTags extends Module {
+    public HealthTags() {
+        super(Addon.CATEGORY, "Health Tags", "Displays other's health above them.");
+    }
+}
