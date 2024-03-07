0a4123432c7bcd4faaacd6a6d37abc8843a4f745
Memeruler_k <104277893+Memeruler-k@users.noreply.github.com>
2024-03-07 16:49:22 +0200

    Create FightBot.java

---
+++ src/main/java/com/example/addon/modules/FightBot.java
@@ -0,0 +1,10 @@
+package com.moon.addon.modules;
+
+import com.moon.addon.Addon;
+import meteordevelopment.meteorclient.systems.modules.Module;
+
+public class FightBot extends Module {
+    public FightBot() {
+        super(Addon.CATEGORY, "Fight Bot", "Automatically fights for you.");
+    }
+}
