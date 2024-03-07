7daa1b5750aca72977c4dd06fb0b0c29dd2d838e
Memeruler_k <104277893+Memeruler-k@users.noreply.github.com>
2024-03-07 16:46:02 +0200

    Create CrashChest.java

---
+++ src/main/java/com/example/addon/modules/CrashChest.java
@@ -0,0 +1,10 @@
+package com.moon.addon.modules;
+
+import com.moon.addon.Addon;
+import meteordevelopment.meteorclient.systems.modules.Module;
+
+public class CrashChest extends Module {
+    public CrashChest() {
+        super(Addon.CATEGORY, "Crash Chest", "Crash server using chests. REQUIRES CREATIVE MODE.");
+    }
+}
