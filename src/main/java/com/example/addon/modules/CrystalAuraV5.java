8b63d54629207143d2c238b590ebac62720b5989
Memeruler_k <104277893+Memeruler-k@users.noreply.github.com>
2024-08-28 12:29:37 +0200

    Update CrystalAuraV5.java

--- src/main/java/com/example/addon/modules/CrystalAuraV5.java
+++ src/main/java/com/example/addon/modules/CrystalAuraV5.java
@@ -1,19 +1,20 @@
 package com.moon.addon.modules;
+
 import meteordevelopment.meteorclient.settings.BoolSetting;
 import meteordevelopment.meteorclient.settings.Setting;
 import meteordevelopment.meteorclient.settings.SettingGroup;
-import meteordevelopment.meteorclient.setting.EnumSetting;
-import meteordevelopment.meteorclient.setting.IntSetting;
-import meteordevelopment.meteorclient.setting.DoubleSetting;
+import meteordevelopment.meteorclient.settings.EnumSetting;
+import meteordevelopment.meteorclient.settings.IntSetting;
+import meteordevelopment.meteorclient.settings.DoubleSetting;
 
 import com.moon.addon.Addon;
 package com.example.addon.modules;
 package meteordevelopment.meteorclient.settings.BoolSetting;
 package meteordevelopment.meteorclient.settings.Setting;
 package meteordevelopment.meteorclient.settings.SettingGroup;
-package meteordevelopment.meteorclient.setting.EnumSetting;
-package meteordevelopment.meteorclient.setting.IntSetting;
-package meteordevelopment.meteorclient.setting.DoubleSetting;
+package meteordevelopment.meteorclient.settings.EnumSetting;
+package meteordevelopment.meteorclient.settings.IntSetting;
+package meteordevelopment.meteorclient.settings.DoubleSetting;
 
 public class CrystalAuraV5 extends Module {
     private final SettingGroup sgGeneral = settings.getDefaultGroup();
